facd55dc31691bb13284ec2a3053404fa45804a0
Max Zilvester <zilimax@gmail.com>
2020-11-17 16:05:23 +0000

    initial commit

---
+++ decorator.js
@@ -0,0 +1,3 @@
+const Decorator = function(paint_stock){
+    this.paint_stock =  paint_stock;
+}
---
+++ room.js
@@ -0,0 +1,7 @@
+const Room = function(area, been_painted) {
+    this.area  = area;
+    this.been_painted = False;
+
+}
+
+module.exports = Room
